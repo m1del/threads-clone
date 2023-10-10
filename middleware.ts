716f9bf2d161040d2980a548bd8d4d59d41b1610
John Miclat <jmiclat@ufl.edu>
2023-10-10 02:13:29 -0400

    fix routes

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
+    // Array of public routers that don't need auth
     publicRoutes:['/','/api/webhook/clerk'],
+
+    // Array of routes to be ignored by auth middleware.
     ignoredRoutes: ['/api/webhook/clerk']
 });
  
